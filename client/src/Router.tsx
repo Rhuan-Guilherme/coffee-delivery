43bb5eaf181d044b932f06e535cf19487124a445
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-12-10 09:59:35 -0300

    Client: Configurando layout de rotas

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { DefaultLayout } from './layouts/defaultLayout';
+import { RegisterLayout } from './layouts/registerLayoyt';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+
+      <Route path="/user" element={<RegisterLayout />}>
+        <Route path="" element={<h1>Register page</h1>} />
+      </Route>
     </Routes>
   );
 }
---
+++ client/src/layouts/registerLayoyt/index.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+
+export function RegisterLayout() {
+  return (
+    <div>
+      <h1>Register Layout</h1>
+      <Outlet />
+    </div>
+  );
+}
